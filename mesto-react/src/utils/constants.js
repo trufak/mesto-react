3a8ddbab984b7f7b93a2cc825572d24af6ae4064
Sergey Trufakin <trufakin.sergey@mail.ru>
2022-09-02 13:37:12 +0700

    fix: исправить замечания по чек-листу

--- mesto-react/src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-//Переменные
-//Переменные для блока profile
-const profile = document.querySelector('.profile');
-export const buttonEditAvatarProfile = profile.querySelector('.profile__edit-avatar-button');
-export const buttonOpenPopupProfile = profile.querySelector('.profile__info-edit-button');
-export const buttonOpenPopupAddCard = profile.querySelector('.profile__add-button');
